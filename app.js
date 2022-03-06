1770cb833a435fee3aa924d66becae78ee9e9aec
Rufino Rojas <rrojas@bizprobpm.ca>
2022-03-06 13:36:48 -0600

    commit initial

---
+++ app.js
@@ -0,0 +1,28 @@
+require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+console.clear();
+
+const main = async () => {
+
+    console.log('HOLA MUNDO!');
+
+    let opt = '';
+    do {
+        opt = await mostrarMenu();
+
+        console.log({opt});
+        await pausa();
+
+    } while (opt !== '0');
+
+
+    /*  pausa(); */
+
+
+
+
+
+}
+
+main();
---
+++ helpers/mensajes.js
@@ -0,0 +1,68 @@
+const { resolve } = require('path');
+
+require('colors');
+
+
+
+const mostrarMenu = () => {
+
+    return new Promise(resolve => {
+        console.clear();
+        console.log('===================================='.magenta);
+        console.log('Seleciones una Opción'.magenta);
+        console.log('==================================== \n'.magenta);
+
+        console.log(`${'1.'.magenta} Crear Tarea`);
+        console.log(`${'2.'.magenta} Listar Tareas`);
+        console.log(`${'3.'.magenta} Listar Tareas Completadas`);
+        console.log(`${'4.'.magenta} Listar Tareas Pendientes `);
+        console.log(`${'5.'.magenta} Completar Tarea(s)`);
+        console.log(`${'6.'.magenta} Borrar Tarea`);
+        console.log(`${'0.'.magenta} Salir \n`);
+
+
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+
+
+        readline.question('Seleciones una opción: ', (opt) => {
+
+            resolve(opt)
+            readline.close();
+
+        })
+    });
+
+
+
+
+
+}
+
+const pausa = () => {
+
+    return new Promise(resolve => {
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        readline.question(`\n Presione ${'ENTER'.blue} para continuar \n`, (opt) => {
+            readline.close();
+            resolve();
+        })
+
+    })
+
+}
+
+
+module.exports = {
+    mostrarMenu,
+    pausa
+}
+
+
+
